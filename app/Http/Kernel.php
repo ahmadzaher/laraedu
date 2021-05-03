35a45a66987a5dd7f1c7e89513bc5d6d57b5c45f
DESKTOP-U04FTGO\User <ahmad.khrezaty@gmail.com>
2021-05-03 05:52:17 +0300

    Add localization to api

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,6 +41,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             'throttle:60,1',
+            'localization',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
@@ -64,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'role' => \App\Http\Middleware\RoleMiddleware::class,
+        'localization' => \App\Http\Middleware\localization::class,
     ];
 }
