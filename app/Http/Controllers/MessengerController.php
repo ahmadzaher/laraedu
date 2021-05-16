0d2f595fe089eb168d67c53d4aa4033a5316c8bb
DESKTOP-U04FTGO\User <ahmadkhrezaty1@gmail.com>
2021-05-16 18:42:38 +0300

    Change messenger webhook request

--- app/Http/Controllers/MessengerController.php
+++ app/Http/Controllers/MessengerController.php
@@ -10,38 +10,24 @@ class MessengerController extends Controller
     {
         $verify_token = $request->hub_verify_token;
         $challenge = $request->hub_challenge;
-        if($verify_token != config('messenger.messenger_verify_token')){
-            return $challenge;
+        if($verify_token == config('messenger.messenger_verify_token')){
+            echo $challenge;
+            exit;
         }
-        $json_response = http_build_query($_POST);
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://5ac5cb97800aeb91bd52fecc4200f283.m.pipedream.net');
-        curl_setopt($ch,CURLOPT_POST,1);
-        curl_setopt($ch,CURLOPT_POSTFIELDS,$json_response);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $reply_response=curl_exec($ch);
+        $payload = json_decode($request->getContent(), true);
+        file_put_contents('test.txt', $payload);
+        //return response()->json( config('messenger.messenger_verify_token'), 200);
     }
     public function webhook_get(Request $request)
     {
         $verify_token = $request->hub_verify_token;
         $challenge = $request->hub_challenge;
-        if($verify_token != config('messenger.messenger_verify_token')){
+        if($verify_token == config('messenger.messenger_verify_token')){
             echo $challenge;
+            exit;
         }
-        $json_response = http_build_query($_GET);
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://5ac5cb97800aeb91bd52fecc4200f283.m.pipedream.net');
-        curl_setopt($ch,CURLOPT_POST,1);
-        curl_setopt($ch,CURLOPT_POSTFIELDS,$json_response);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $reply_response=curl_exec($ch);
-
+        $payload = json_decode($request->getContent());
+        file_put_contents('test.txt', $payload);
         //return response()->json( config('messenger.messenger_verify_token'), 200);
     }
 }
