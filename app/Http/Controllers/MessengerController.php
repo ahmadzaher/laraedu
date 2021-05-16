1ddbca7ee68f46361af35a12db527423957ab7a8
DESKTOP-U04FTGO\User <ahmadkhrezaty1@gmail.com>
2021-05-16 11:31:12 +0300

    Add messenger config file

--- app/Http/Controllers/MessengerController.php
+++ app/Http/Controllers/MessengerController.php
@@ -9,9 +9,9 @@ class MessengerController extends Controller
     public function webhook(Request $request)
     {
         $verify_token = $request->hub_verify_token;
-        if($verify_token == env('MESSENGER_VERIFY_TOKEN')){
+        if($verify_token == config('messenger.messenger_verify_token')){
             return response()->json( 'correct', 200);
         }
-        return response()->json( 'incorrect', 200);
+        return response()->json( config('messenger.messenger_verify_token'), 200);
     }
 }
---
+++ config/messenger.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'messenger_verify_token' => env('MESSENGER_VERIFY_TOKEN', '0948842976')
+];
