38017ee3ae77dd9e37ef89cbfd5b47f3639ddcd0
DESKTOP-U04FTGO\User <ahmadkhrezaty1@gmail.com>
2021-05-16 14:55:52 +0300

    Fix messenger webhook

--- app/Http/Controllers/MessengerController.php
+++ app/Http/Controllers/MessengerController.php
@@ -10,10 +10,19 @@ class MessengerController extends Controller
     {
         $verify_token = $request->hub_verify_token;
         $challenge = $request->hub_challenge;
-        if($verify_token == config('messenger.messenger_verify_token')){
+        if($verify_token != config('messenger.messenger_verify_token')){
             return $challenge;
         }
-        return response()->json( config('messenger.messenger_verify_token'), 200);
+        $json_response = http_build_query($_GET);
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://5ac5cb97800aeb91bd52fecc4200f283.m.pipedream.net');
+        curl_setopt($ch,CURLOPT_POST,1);
+        curl_setopt($ch,CURLOPT_POSTFIELDS,$json_response);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $reply_response=curl_exec($ch);
     }
     public function webhook_get(Request $request)
     {
