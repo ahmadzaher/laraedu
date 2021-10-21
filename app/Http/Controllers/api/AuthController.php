e753859bf46ace9a67151673d1e7d664985da117
DESKTOP-U04FTGO\User <ahmadkhrezaty1@gmail.com>
2021-10-21 22:42:21 +0300

    Change /user get request

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -104,7 +104,43 @@ class AuthController extends Controller
         unset($user->number);
         $user->avatar = $avatar;
 
-        return response()->json(['user' => $user], 200);
+        $user_roles = [];
+
+        $user_permissions = [];
+
+        foreach($user->roles as $role){
+            if($role->slug == 'student' or $role->slug == 'teacher'){
+                return redirect('/user')->with('warning', 'Something went Wrong');
+            }
+            $user_roles[] = [
+                'id' => $role->id,
+                'name' => $role->name,
+                'slug' => $role->slug
+            ];
+            foreach ( $role->permissions as $key => $permission )
+            {
+                $permission_data = [
+                    'id' => $permission->id,
+                    'name' => $permission->name,
+                    'slug' => $permission->slug
+                ];
+                if(in_array($permission_data, $user_permissions))
+                    continue;
+                $user_permissions[] = $permission_data;
+            }
+        }
+        $user->phone_number = $user->number;
+        $avatar = $user->getFirstMediaUrl('avatars', 'thumb') ? url($user->getFirstMediaUrl('avatars', 'thumb')) : url('/images/avatar.jpg') ;
+
+        return response()->json([
+            'id' => $user->id,
+            'email' => $user->email,
+            'username' => $user->username,
+            'phone_number' => $user->phone_number,
+            'avatar' => $avatar,
+            'roles' => $user_roles,
+            'permissions' => $user_permissions
+        ], 200);
 
     }
 
