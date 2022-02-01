c1e2156d6d0823e4b2bb078c82842830e087ed5e
Ahmad Zaher Khrezaty <ahmad.khrezaty@gmail.com>
2022-02-02 00:55:15 +0200

    Fix favicon and logo

--- app/Http/Controllers/api/v1/SettingsController.php
+++ app/Http/Controllers/api/v1/SettingsController.php
@@ -15,8 +15,8 @@ class SettingsController extends Controller
             'default_language' => option('default_language'),
             'default_direction' => option('default_direction'),
             'app_name' => option('app_name'),
-            'app_logo' => option('app_logo') != '' ? env('APP_URL') . option('app_logo') : '',
-            'app_favicon' => option('app_favicon') != '' ? env('APP_URL') . option('app_favicon') : '',
+            'app_logo' => option('app_logo'),
+            'app_favicon' => option('app_favicon'),
         ];
         return response()->json($settings, 200);
     }
@@ -31,18 +31,29 @@ class SettingsController extends Controller
             'app_logo' => 'image|mimes:jpg,png,jpeg,gif,svg|max:2048',
             'app_favicon' => 'image|mimes:jpg,png,jpeg,gif,svg|max:2048',
         ]);
+        if ($request->delete_logo){
+            $files = Storage::allFiles('public/images/app_logo');
+            Storage::delete($files);
+            option(['app_logo' => '']);
+        }
+        if ($request->delete_favicon){
+            $files = Storage::allFiles('public/images/app_favicon');
+            Storage::delete($files);
+            option(['app_favicon' => '']);
+        }
         $files = ['app_logo', 'app_favicon'];
         foreach ($files as $file)
+        {
             if ($request->hasFile($file)) {
                 if ($request->file($file)->isValid()) {
                     $files = Storage::allFiles('public/images/'.$file);
                     Storage::delete($files);
-                        $extension = $request->$file->extension();
-                        $request->$file->storeAs('/public/images/'.$file, $file.".".$extension);
-                        $url = Storage::url($file.".".$extension);
-                        option([$file => $url]);
+                    $extension = $request->$file->extension();
+                    $request->$file->storeAs('/public/images/'.$file, $file.".".$extension);
+                    option([$file => env('APP_URL') . '/storage/images/'.$file . '/' . $file . '.' . $extension]);
                 }
             }
+        }
         option(['default_language' => $request->default_language]);
         option(['default_direction' => $request->default_direction]);
         option(['app_name' => $request->app_name]);
