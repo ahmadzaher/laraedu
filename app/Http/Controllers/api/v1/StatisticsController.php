5e8e721ac49839c8def5202262aac67cba6511c1
Ahmad Zaher Khrezaty <ahmad.khrezaty@gmail.com>
2022-09-19 22:42:56 +0300

    Latest users request

--- app/Http/Controllers/api/v1/StatisticsController.php
+++ app/Http/Controllers/api/v1/StatisticsController.php
@@ -294,6 +294,7 @@ class StatisticsController extends Controller
             $avatar = $staff->getFirstMediaUrl('avatars', 'thumb') != null ? url($staff->getFirstMediaUrl('avatars', 'thumb')) : url('/images/avatar.jpg');
 
             $users[$key]['avatar'] = $avatar;
+            $users[$key]['last_login'] = Carbon::parse($users[$key]['last_login'])->diffForHumans();
             unset($users[$key]['media']);
         }
         return response($users);
