018f57efd3c889c94fca59228d7983be00d923de
Ahmad Zaher Khrezaty <ahmad.khrezaty@gmail.com>
2022-10-07 19:31:20 +0300

    Test email logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,11 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-        <img src="{{ option('app_logo') }}" class="logo" alt="Laravel Logo">
-
+@if (trim($slot) === 'Laravel')
+<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+@else
+{{ $slot }}
+@endif
 </a>
 </td>
 </tr>
