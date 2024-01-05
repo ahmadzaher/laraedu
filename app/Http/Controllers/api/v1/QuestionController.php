00f192bfd24dae4ae366fdf8e45f30b2de32d512
Ahmad Zaher Khrezaty <ahmad.khrezaty@gmail.com>
2024-01-06 00:30:43 +0300

    Split the content to smaller texts

--- app/Http/Controllers/api/v1/QuestionController.php
+++ app/Http/Controllers/api/v1/QuestionController.php
@@ -115,116 +115,124 @@ class QuestionController extends Controller
         $content = str_replace('</w:r></w:p>', "\r\n", $content);
         $striped_content = strip_tags($content);
 
-        $openai_api_key = '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';
+        $openai_api_key = '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';
         $api_url = 'https://api.openai.com/v1/chat/completions'; // استخدام نموذج GPT-3
 
-
-        $data = array(
-            'model' => 'gpt-3.5-turbo',
-            'temperature' => 0.1,
-            "messages" => [
-                [
-                    "role" => "system",
-                    "content" => ' أنت محترف في كتابة الامتحانات المؤتمتة بحيث عند ارسال المستخدم نص تقوم باستخراج عدة اسئلة منه ولكل سؤال 4 اجابات وتقوم بطباعة الأسئلة والأجوبة بنفس لغة النص الذي أعطاه لك المستخدم واخراج جميع الأسئلة المحتمل ورودها في الامتحان النائي "بصيغة json وبهذا الشكل حصرا"
+        $words = explode(' ', $striped_content);
+        $number_of_words = count($words);
+        $counts = $number_of_words > 100 ? $number_of_words / 100 : 1;
+        for($i = 1; $i <= $counts; $i++)
+        {
+            $content = implode(' ', array_slice(explode(' ', $striped_content), $i * 100, ($i * 100) + 100));
+            $data = array(
+                'model' => 'gpt-3.5-turbo',
+                'temperature' => 0.1,
+                "messages" => [
+                    [
+                        "role" => "system",
+                        "content" => ' أنت محترف في كتابة الامتحانات المؤتمتة بحيث عند ارسال المستخدم نص تقوم باستخراج عدة اسئلة منه ولكل سؤال 4 اجابات وتقوم بطباعة الأسئلة والأجوبة بنفس لغة النص الذي أعطاه لك المستخدم واخراج جميع الأسئلة المحتمل ورودها في الامتحان النائي "بصيغة json وبهذا الشكل حصرا"
                     {
 "questions": [
     {
-        "type": "single",
-        "active": 1,
-        "level": "1",
-        "score": "1",
-        "default_time": "50",
         "content": "كم عدد الطلاب",
         "answers": [
             {
-                "question_id": null,
-                "active": 1,
                 "correct": "0",
                 "content": "طالبان"
             },
             {
-                "question_id": null,
-                "active": 1,
                 "correct": "1",
                 "content": "ثلاثة طلاب"
+            },
+            {
+                "correct": "0",
+                "content": "أربقة طلاب"
+            },
+            {
+                "correct": "0",
+                "content": "خمسة طلاب"
             }
         ]
     }
 ]}
-بحيث تكون القيمة correct هي 1 في حال كانت الاجابة صحيحة ويجب وضع السؤال الصحيح بمكان مختلف ضمن المصفوفة في كل مرة ويجب عدم الخروج عن النص المكتوب أبدا ويجب عدم انهاء الجواب حتى اكمال المصفوفة '
-                ],
-                [
-                    "role" => "user",
-                    "content" => $striped_content
+بحيث تكون القيمة correct هي 1 في حال كانت الاجابة صحيحة ويجب وضع السؤال الصحيح
+ بمكان مختلف ضمن المصفوفة في كل مرة ويوجد اجابة صحيحة دائما ويجب عدم الخروج عن النص المكتوب أبدا ويجب عدم انهاء الجواب حتى اكمال المصفوفة "بصيغة json وبهذا الشكل حصرا"'
+                    ],
+                    [
+                        "role" => "user",
+                        "content" => $content
+                    ]
                 ]
-            ]
-        );
+            );
 
-        $headers = array(
-            'Content-Type: application/json',
-            'Authorization: Bearer ' . $openai_api_key,
-        );
+            $headers = array(
+                'Content-Type: application/json',
+                'Authorization: Bearer ' . $openai_api_key,
+            );
 
-        $ch = curl_init($api_url);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $ch = curl_init($api_url);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-        $response = curl_exec($ch);
+            $response = curl_exec($ch);
 
-        if (curl_errno($ch)) {
-            echo 'Error:' . curl_error($ch);
-        }
-
-        curl_close($ch);
-
-        $decoded_response = json_decode($response, true);
-// النص المولد
-        $generated_text = json_decode($decoded_response['choices'][0]['message']['content']);
-        foreach ($generated_text->questions as $question)
-        {
-            if (!is_array($question->answers))
-                return Response(['msg' => 'No Answers'], 422);
-
-            $new_question = new Question([
-                'type' => 'single',
-                'active' => 1,
-                'level' => $question->level,
-                'score' => $question->score,
-                'default_time' => $question->default_time,
-                'content' => $question->content,
-                'group_id' => $request->group,
-                'solution' => '',
-                'hint' => '',
-                'branch_id' => $request->branch_id,
-                'subject_id' => $request->subject_id,
-                'seller_id' => $request->seller_id,
-                'year' => $request->year,
-            ]);
-
-            $correct_exists = 0;
-            foreach ($new_question->answers as $answer) {
-                if ($answer['correct'] && $correct_exists)
-                    return Response(['msg' => 'Choose just one correct answer'], 422);
-                if ($answer['correct'] == 1)
-                    $correct_exists = 1;
+            if (curl_errno($ch)) {
+                echo 'Error:' . curl_error($ch);
             }
 
-            $new_question->save();
+            curl_close($ch);
 
+            $decoded_response = json_decode($response, true);
+            $generated_text = json_decode($decoded_response['choices'][0]['message']['content']);
+            foreach ($generated_text->questions as $question)
+            {
+                if (!is_array($question->answers))
+                    return Response(['msg' => 'No Answers'], 422);
 
-            foreach ($question->answers as $answer) {
-                $answer = new Answer([
-                    'question_id' => $new_question->id,
+                $new_question = new Question([
+                    'type' => 'single',
                     'active' => 1,
-                    'correct' => $answer->correct,
-                    'content' => $answer->content
+                    'level' => 1,
+                    'score' => 1,
+                    'default_time' => 50,
+                    'content' => $question->content,
+                    'group_id' => $request->group_id,
+                    'solution' => '',
+                    'hint' => '',
+                    'branch_id' => $request->branch_id,
+                    'subject_id' => $request->subject_id,
+                    'seller_id' => $request->seller_id,
+                    'year' => $request->year,
                 ]);
-                $answer->save();
+
+                $correct_exists = 0;
+                foreach ($new_question->answers as $answer) {
+                    if ($answer['correct'] && $correct_exists)
+                        return Response(['msg' => 'Choose just one correct answer'], 422);
+                    if ($answer['correct'] == 1)
+                        $correct_exists = 1;
+                }
+
+                $new_question->save();
+
+
+                foreach ($question->answers as $answer) {
+                    $answer = new Answer([
+                        'question_id' => $new_question->id,
+                        'active' => 1,
+                        'correct' => $answer->correct,
+                        'content' => $answer->content
+                    ]);
+                    $answer->save();
+                }
             }
         }
-        return response(['data' => ($generated_text), 'message' => 'success']);
+
+
+
+        return response(['message' => 'success']);
 
     }
 
